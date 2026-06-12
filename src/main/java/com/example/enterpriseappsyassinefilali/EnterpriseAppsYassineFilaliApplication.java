9f1b6166f8ab48787c6aab0c4956d07730ab194e
Yassine1000L <yassneyas11@gmail.com>
2026-06-12 02:33:27 +0200

    chore: initial project setup (Maven wrapper, Spring Boot app, base config)

---
+++ src/main/java/com/example/enterpriseappsyassinefilali/EnterpriseAppsYassineFilaliApplication.java
@@ -0,0 +1,13 @@
+package com.example.enterpriseappsyassinefilali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAppsYassineFilaliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseAppsYassineFilaliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/enterpriseappsyassinefilali/EnterpriseAppsYassineFilaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enterpriseappsyassinefilali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseAppsYassineFilaliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

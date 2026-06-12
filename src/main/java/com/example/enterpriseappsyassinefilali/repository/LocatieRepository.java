3654c4e57646370ecafc2468fd6aa44df2225258
Yassine1000L <yassneyas11@gmail.com>
2026-06-12 03:09:00 +0200

    feat: LocatieRepository (CrudRepository)

---
+++ src/main/java/com/example/enterpriseappsyassinefilali/repository/LocatieRepository.java
@@ -0,0 +1,7 @@
+package com.example.enterpriseappsyassinefilali.repository;
+
+import com.example.enterpriseappsyassinefilali.model.Locatie;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LocatieRepository extends CrudRepository<Locatie, Long> {
+}
